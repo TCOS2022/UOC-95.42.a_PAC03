4805d3602321dff86589ab5fb0dc8e417934ff4a
TCOS <jsanpons@uoc.edu>
2022-10-24 22:04:19 +0200

    update: pagina de error. transferit styls al plantilla_layout

--- src/views/pages/pageError.vue
+++ src/views/pages/pageError.vue
@@ -1,16 +1,27 @@
 <template>
-    <div class="container>">
-        <h1>PAGINA DE ERROR</h1>
+    <div class="pageContainer">
+        <header></header>
+        <main>
+            <div class="loader"></div>
+            <div class="contents">
+                <div class="contentPageError">
+                    <h1>PAGINA DE ERROR</h1>
 
-        <p>Ho sentim molt, pero estas buscant un recurs que no tenim controlat </p>
+                    <p>Ho sentim molt, pero estas buscant un recurs que no tenim controlat </p>
 
-        <img src="media/imatges/fails/picard_fails.webp" alt="">
+                    <img src="media/imatges/fails/Picard_fails.webp" alt="">                    
+                </div>
+            </div>
+        </main>
+        <footer></footer>
+
     </div>
 </template>
 
 <script></script>
 
-<style scoped>
+<style>
+/* estils passats a plantilla_layout 
     div{
         width:75%;
         border: solid red 10px;
@@ -22,4 +33,5 @@
     img{width: 50%; margin:0 auto; text-align:center}
     h1{width: 75%;margin:0 auto; padding: 1rem; background-color: red;color:white;font-size: 3em;}
     p{width: 75%;margin:2rem auto; padding: 1rem; background-color: blue;color:yellow;font-size: 2em;}
+*/
 </style>
